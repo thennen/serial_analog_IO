db76b1801df468729f503771e7382676779aa2a7
Tyler <tyler@hennen.us>
2017-07-23 18:38:28 +0200

    Basically works.  Limited to 8 bit output.

---
+++ serial_analog_IO.ino
@@ -0,0 +1,93 @@
+#include <CmdMessenger.h>
+
+int analogWriteValue;
+int analogReadValue;
+int analogWriteChannel;
+int analogReadChannel;
+
+// Attach a new CmdMessenger object to the default Serial port
+CmdMessenger cmdMessenger = CmdMessenger(Serial);
+
+// This is the list of recognized commands. These can be commands that can either be sent or received.
+enum
+{
+  AO                   , // Command to set analog out
+  AI                   , // Command to read analog input
+  kStatus              , // Command to report status
+};
+
+// Callbacks define on which received commands we take action
+void attachCommandCallbacks()
+{
+  // Attach callback methods
+  cmdMessenger.attach(OnUnknownCommand);
+  cmdMessenger.attach(AO, AnalogOutput);
+  cmdMessenger.attach(AI, AnalogInput);
+}
+
+// Callback function that changes the analog output level
+void AnalogOutput()
+{
+  analogWriteChannel = cmdMessenger.readInt16Arg();
+  analogWriteValue = cmdMessenger.readInt16Arg();
+  analogWrite(analogWriteChannel, analogWriteValue);
+  cmdMessenger.sendCmdStart(kStatus);
+  cmdMessenger.sendCmdArg("Wrote some voltage to analog out");
+  cmdMessenger.sendCmdArg(analogWriteValue);
+  cmdMessenger.sendCmdEnd();
+}
+
+// Callback function that reads the analog input
+void AnalogInput()
+{
+  analogReadChannel = cmdMessenger.readInt16Arg();
+  analogReadValue = analogRead(analogReadChannel);
+  
+  cmdMessenger.sendCmdStart(kStatus);
+  cmdMessenger.sendCmdArg("Measured analog voltage");
+  cmdMessenger.sendCmdArg(analogReadValue);
+  cmdMessenger.sendCmdEnd();
+}
+
+// Called when a received command has no attached function
+void OnUnknownCommand()
+{
+  cmdMessenger.sendCmd(kStatus,"Command without attached callback");
+}
+
+// Setup function
+void setup()
+{
+  // Listen on serial connection for messages from the PC
+  Serial.begin(9600); 
+
+  // Adds newline to every command
+  cmdMessenger.printLfCr();   
+
+  // Attach my application's user-defined callback methods
+  attachCommandCallbacks();
+
+  // Set all digital pins for output
+  //pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+}
+
+// Loop function
+void loop() 
+{
+  // Process incoming serial data, and perform callbacks
+  cmdMessenger.feedinSerialData();
+}
+
+
